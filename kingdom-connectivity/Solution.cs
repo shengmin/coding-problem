d0f49a5264d0cafd16d8c94a6a9b6de0225b32f2
ShengMin Zhang <me@shengmin.me>
2012-03-20 19:52:53 -0400

    change kindom connectivity solution

--- kingdom-connectivity/Solution.cs
+++ kingdom-connectivity/Solution.cs
@@ -8,73 +8,174 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
+public struct Natural {
+	public static readonly Natural Infinity = new Natural(VALUE_INFINITY);
+	private const int VALUE_INFINITY = -1;
+	private int val;
+
+	public Natural(int val) {
+		this.val = val;
+	}
+	
+	public int Value {
+		get { return val; }
+	}
+	
+	public bool IsInfinity() {
+		return val == VALUE_INFINITY;
+	}
+	
+	public static Natural operator + (Natural n1, Natural n2) {
+		if(n1.IsInfinity() || n2.IsInfinity()) return Infinity;
+		return n1.val + n2.val;
+	}
+	
+	public static Natural operator % (Natural n1, Natural n2) {
+		return n1.val % n2.val;
+	}
+	
+	public static implicit operator Natural(int v) {
+		return new Natural(v);
+	}
+	
+	public static implicit operator int(Natural n) {
+		return n.val;
+	}
+	
+	public override string ToString() {
+		return val.ToString();
+	}
+}
+
+public class City {
+	public int Id { get; private set; }
+	public Natural PathCount { get; set; } // number of paths from this city to warfare capital
+	public ICollection<City> Neighbours { get; private set; }
+	
+	public City(int id) {
+		Id = id;
+		PathCount = 0;
+		Neighbours = new LinkedList<City>();
+	}
+	
+	public void Add(City c) {
+		Neighbours.Add(c);
+	}
+	
+	public static bool operator == (City c1, City c2) {
+		return c1.Id == c2.Id;
+	}
+	
+	public static bool operator != (City c1, City c2) {
+		return !(c1 == c2);
+	}
+	
+	public static implicit operator int(City c) {
+		return c.Id;
+	}
+	
+	public override bool Equals(object o) {
+		return (o is City) && (City)o == this;
+	}
+	
+	public override int GetHashCode(){
+		return Id;
+	}
+}
+
 public class Solution {
 	int N, M;
-	IDictionary<int, ICollection<int>> cityNeighbours; // cityNeighbours[i] = list of neighbours of city i
-	IDictionary<int, int> pathCount; // pathCount[i] = number of paths from city i to warfare capital if non-negative, -1 indicates infinite number of paths, -2 indicates cycle
-	bool[] visited; // visited[i] is true iff city i has been visited
-	const int MOD = 1000000000;
-
-	int Dfs(int city) {
-		if(city == N) return 1;
-		if(pathCount.ContainsKey(city)) return pathCount[city];
-		if(visited[city]) return -2; // a cycle
+	readonly Natural MOD = 1000000000;
+	bool[] visited; 
+	bool[] visited2;
+	Stack<City> visitedCities;
+	City[] cities;
+	City firstCity, lastCity;
 	
-		visited[city] = true;
-		int totalCount = 0;
-		bool hasCycle = false;
-		ICollection<int> neighbours = null;
-		cityNeighbours.TryGetValue(city, out neighbours);
+	/**
+	 * Test whether not or not it's reachable from city to city to
+	 */
+	bool IsReachable(City city, City to) {
+		if(city == to) return true; // reach the destination city
+		if(visited2[city]) return false; // a cycle;
 		
-		if(neighbours == null) return 0; // no way to make to the warfare capital
+		visited2[city] = true;
 		
-		foreach(int neighbour in neighbours) {
-			int count = Dfs(neighbour);
-			if(count == -2) {
-				hasCycle = true;
-			} else if(count == -1) {
-				totalCount = -1;
+		bool anyReachable = false;
+		foreach(City neighbour in city.Neighbours) {
+			bool reachable = IsReachable(neighbour, to);
+			if(reachable) {
+				anyReachable = true;
 				break;
-			} else {
-				totalCount = (totalCount + count) % MOD;
 			}
 		}
 		
-		if(hasCycle && totalCount > 0) totalCount = -1;
-		pathCount[city] = totalCount;
+		visited2[city] = false;
+		
+		return anyReachable;
+	}
+	
+	Natural CountPath(City city) {
+		if(visited[city]) {
+			// a cycle
+			foreach(City c in visitedCities) {
+				// if any of the city in the cycle can reach warfare capital, then there's infinite path, otherwise 0
+				bool reachable = IsReachable(c, lastCity);
+				if(reachable) return Natural.Infinity;
+				if(c == city) break;
+			}
+			return 0;
+		}
+		
+		visited[city] = true;
+		visitedCities.Push(city);
+		ICollection<City> neighbours = city.Neighbours;
+		
+		Natural totalCount = city == lastCity ? 1 : 0;
+		foreach(City n in neighbours) {
+			totalCount += CountPath(n);
+			if(totalCount.IsInfinity()) break;
+		}
+		
+		visitedCities.Pop();
+		visited[city] = false;
 		
 		return totalCount;
 	}
+	
 	
 	void Run(TextReader rd) {
 		string[] ln = rd.ReadLine().Split(' ');
 		N = int.Parse(ln[0]);
 		M = int.Parse(ln[1]);
 		visited = new bool[N + 1];
-		cityNeighbours = new Dictionary<int, ICollection<int>>(N);
-		pathCount = new Dictionary<int, int>(N);
+		visited2 = new bool[N + 1];
+		visitedCities = new Stack<City>(N);
+		cities = new City[N + 1];
+		
+		for(int i = N; i > 0; i--) {
+			City c = new City(i);
+			cities[i] = c;
+		}
+		
+		firstCity = cities[1];
+		lastCity = cities[N];
 		
 		for(int i = 0; i < M; i++) {
 			ln = rd.ReadLine().Split(' ');
-			int x = int.Parse(ln[0]);
-			int y = int.Parse(ln[1]);
-			ICollection<int> neighbours = null;
-			cityNeighbours.TryGetValue(x, out neighbours);
-			if(neighbours == null) {
-				neighbours = new LinkedList<int>();
-				cityNeighbours[x] = neighbours;
-			}
-			neighbours.Add(y);
+			int from = int.Parse(ln[0]);
+			int to = int.Parse(ln[1]);
+			cities[from].Add(cities[to]);
 		}
 		
 		rd.Dispose();
 		
-		int result = Dfs(1);
+		Natural count = CountPath(firstCity);
 		
-		if(result == -1) {
+		if(count.IsInfinity()) {
 			Console.WriteLine("INFINITE PATHS");
 		} else {
-			Console.WriteLine(result);
+			Console.WriteLine(count);
 		}
 	}
 
