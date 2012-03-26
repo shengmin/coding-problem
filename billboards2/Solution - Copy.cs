9f846963b88986efc251654a0ac0c39df9184fa4
ShengMin Zhang <me@shengmin.me>
2012-03-26 01:04:15 -0400

    optimize billboards2 solution

--- billboards2/Solution - Copy.cs
+++ billboards2/Solution - Copy.cs
@@ -12,14 +12,16 @@
  using System.IO;
  using System.Collections.Generic;
  
- 
- 
 
  
  public class Solution {
 	int N, K;
 	int[] boards;
 	long[][] cols = new long[2][];
+	
+	const int FULL = 2;
+	const int ZERO = 0;
+	const int NOT_FULL = 1;
  
 	void Swap() {
 		long[] a = cols[0];
@@ -33,40 +35,41 @@
 		K = int.Parse(ln[1]);
 		
 		boards = new int[N + 1];
-		long[] col = cols[0] = new long[K + 1];
-		cols[1] = new long[K + 1];
+		long[] col = cols[0] = new long[3];
+		cols[1] = new long[3];
 		
 		for(int i = 1; i <= N; i++) {
 			boards[i] = int.Parse(rd.ReadLine());
 		}
 		
-		long sum = 0; // sum up last K
-		int start = N;
-		
-		for(int i = K; i > 0; i--, start--) {
-			sum += boards[start];
+		for(int c = ZERO; c <= FULL; c++) {
+			if(c == FULL) col[c] = 0;
+			else col[c] = boards[N];
 		}
 		
-		col[K] = sum;
-		
-		for(int i = K - 1, j = 1; i >= 0; i--, j++) {
-			col[i] = Math.Max(col[i + 1], sum + boards[start] - boards[j + start]);
-		}
-		
-		for(int j = start - 1; j > 0; j--) {
+		for(int j = N - 1; j > 0; j--) {
 			long[] pre = cols[0];
 			long[] cur = cols[1];
-			cur[K] = pre[0];
 			int board = boards[j];
 		
-			for(int i = K - 1; i >= 0; i--) {
-				cur[i] = Math.Max(pre[0], board + pre[i + 1]);
+			for(int i = FULL; i >= ZERO; i--) {
+				switch(i) {
+					case ZERO:
+						cur[i] = Math.Max(board + pre[NOT_FULL], Math.Max(pre[ZERO], board + pre[FULL]));
+						break;
+					case NOT_FULL:
+						cur[i] = Math.Max(board + pre[FULL], Math.Max(pre[NOT_FULL], board + pre[NOT_FULL]));
+						break;
+					case FULL:
+						cur[i] = pre[ZERO];
+						break;
+				}
 			}
 			
 			Swap();
 		}
 	
-		Console.WriteLine(cols[0][0]);
+		Console.WriteLine(cols[0][ZERO]);
 	}
  
 	public static void Main(string[] args) {
