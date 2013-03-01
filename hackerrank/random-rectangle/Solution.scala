87c9b00af4029ffe26776c788401cd6b446b625a
ShengMin Zhang <me@shengmin.me>
2013-03-01 02:29:56 -0800

    first attempt to solve random rectangle

--- hackerrank/random-rectangle/Solution.scala
+++ hackerrank/random-rectangle/Solution.scala
@@ -1,15 +1,8 @@
 import java.io._
-import java.util.{StringTokenizer}
-import scala.collection.mutable.HashMap
+import java.util.{StringTokenizer, BitSet}
+import scala.collection.mutable.{HashMap, MutableList, Map}
 
 object Solution {
-  object Stage extends Enumeration {
-    type Stage = Value
-    val Width, Height = Value
-  }
-
-  import Stage._
-  
   val reader = new BufferedReader(new InputStreamReader(System.in))
   val N = reader.readLine().trim().toInt
   val sticks = new Array[Int](N)
@@ -20,46 +13,101 @@ object Solution {
   }
   
   val R = reader.readLine().trim().toInt
-  val table = HashMap.empty[String, (Int, Double)]
-  var expectedArea: Double = 0.0D
-
-  def getKey(width: Int, height: Int, usedSticks: BitSet, stage: Stage) = {
-    "%d,%d,%d,%d".format(width, height, usedSticks.toLongArray()(0), stage.id)
-  }
+  val widthTable = HashMap.empty[Int, MutableList[(Int, Int, Double)]]
+  val heightTable = HashMap.empty[Long, MutableList[(Int, Int, Double)]]
   
-  def solveWidth(width: Int, height: Int, usedSticks: BitSet): List[(Int, Double)] = {
-    val stickId = usedSticks.nextClearBit();
-    if (stickId == N || width > R) {
-      return Nil
+  def log[E](a: Array[Array[E]]) {
+    for (row <- a) {
+      for (i <- row) {
+        print(i)
+        print(' ')
+      }
+      println()
     }
-    
-    val list1 = solve(width + sticks(stickId), height
-    
-    //val (a1, p1) = solve(width + sticks(stickId), height, stickId + 1)
-    return null
   }
   
-  def solveHeight(width: Int, height: Int, stickId: Int): (Int, Double) = {
-    return null
-  }
-  
-  def solve(width: Int, height: Int, stickId: Int, stage: Stage): (Int, Double) = {
-    val key = getKey(width, height, stickId, stage)
-    table.get(key) match {
+  def solveWidthStage(usedSticks: BitSet, stickId: Int): MutableList[(Int, Int, Double)] = {
+    return widthTable.get(stickId) match {
       case Some(x) => x
       case None => {
-        val result = stage match {
-          case Width => solveWidth(width, height, stickId)
-          case Height => solveHeight(width, height, stickId)
+        if (stickId >= N) return MutableList((0, 0, 1.0D))
+      
+        val list = MutableList.empty[(Int, Int, Double)]
+        val nextStickId = stickId + 1
+        val usedSticks1 = usedSticks.clone().asInstanceOf[BitSet]
+        usedSticks1.set(stickId)
+        val heightList = solveHeightStage(usedSticks1, N - nextStickId)
+        val widthList = solveWidthStage(usedSticks1.clone().asInstanceOf[BitSet], nextStickId)
+         
+        // The robot eats the stick and stays at width stage
+        for ((w, h, p) <- widthList) {
+          if (w <= R) list += ((w, h, p * 0.25D))
         }
-        table(key) = result
-        result
+        
+        // The robot eats the stick and goes to height stage
+        for ((w, h, p) <- heightList) {
+          if (w <= R) list += ((w, h, p * 0.25D))
+        }
+        
+        // The robot takes the stick and stays at width stage
+        for ((w, h, p) <- widthList) {
+          val newWidth = w + sticks(stickId);
+          if (newWidth <= R) list += ((newWidth, h, p * 0.25D))
+        }
+        
+        // The robot takes the stick and goes to height stage
+        for ((w, h, p) <- heightList) {
+          val newWidth = w + sticks(stickId);
+          if (newWidth <= R) list += ((newWidth, h, p * 0.25D))
+        }
+        
+        widthTable(stickId) = list
+        list
+      }
+    }
+  }
+  
+  def solveHeightStage(usedSticks: BitSet, stickCount: Int): MutableList[(Int, Int, Double)] = {
+    val key = usedSticks.toLongArray()(0)
+    return heightTable.get(key) match {
+      case Some(x) => x
+      case None => {
+        if (stickCount == 0) return MutableList((0, 0, 1.0D))
+      
+        val stickId = usedSticks.nextClearBit(0)
+        val list = MutableList.empty[(Int, Int, Double)]
+        
+        var i = stickId
+        while (i < N) {
+          val newUsedSticks = usedSticks.clone().asInstanceOf[BitSet]
+          newUsedSticks.set(i)
+          
+          for ((w, h, p) <- solveHeightStage(newUsedSticks, stickCount - 1)) {
+            val newHeight = h + sticks(stickId)
+            if (newHeight <= R) list += ((w, newHeight, p / stickCount))
+          }
+          
+          i = usedSticks.nextClearBit(i + 1)
+        }
+        
+        heightTable(key) = list
+        list
       }
     }
   }
   
   def main(a: Array[String]) {
-    //solve(0, 0, 0, Stage.Width)
-    println(expectedArea)
+    var area = 0.0D
+    val list = solveWidthStage(new BitSet(N), 0)
+    for ((w, h, p) <- list) {
+      area = area + w * h * p
+    }
+    println(area)
+    println()
+    println(list)
+    println()
+    println(widthTable)
+    println()
+    println(heightTable)
   }
 }
