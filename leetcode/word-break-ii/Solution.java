4a2706ee871b6d795d37cbe8badd219229de602f
ShengMin Zhang <me@shengmin.me>
2013-11-18 01:09:58 -0800

    Add solution to word break II

--- leetcode/word-break-ii/Solution.java
+++ leetcode/word-break-ii/Solution.java
@@ -3,39 +3,61 @@ import java.util.*;
 public class Solution {
   public static void main(String[] args) {
     Solution s = new Solution();
-    System.out.println(s.wordBreak("catsanddog", new HashSet<String>(){{
-        add("cat");
-        add("cats");
-        add("and");
-        add("sand");
-        add("dog");
-    }}));
+    String[] words = new String[] { "a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa" };
+    System.out.println(s.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", new HashSet<String>(Arrays.asList(words))));
+    String[] words2 = new String[] { "cat", "cats", "and", "sand", "dog" };
+    System.out.println(s.wordBreak("catsanddog", new HashSet<String>(Arrays.asList(words2))));
   }
 
   public ArrayList<String> wordBreak(String s, Set<String> dict) {
     int length = s.length();
-    ArrayList<ArrayList<String>> table =
-      new ArrayList<ArrayList<String>>(length + 1);
+    ArrayList<List<Integer>> table =
+      new ArrayList<List<Integer>>(length + 1);
 
     for (int i = 0; i <= length; i++) {
-      table.add(new ArrayList<String>());
+      table.add(new ArrayList<Integer>());
     }
 
-    table.get(0).add("");
+    table.get(0).add(-1);
 
     for (int i = 1; i <= length; i++) {
-      ArrayList<String> newList = table.get(i);
+      List<Integer> newList = table.get(i);
       for (int j = i; j >= 0; j--) {
-        ArrayList<String> list = table.get(j);
+        List<Integer> list = table.get(j);
         String word = s.substring(j, i);
-        for (String sentence: list) {
-          if (dict.contains(word)) {
-            newList.add(sentence + (sentence.length() > 0 ? " " : "") + word);
-          }
+        if (list.size() > 0 && dict.contains(word)) {
+          newList.add(j);
         }
       }
     }
 
-    return table.get(length);
+    ArrayList<String> words = new ArrayList<String>();
+    findWords(s, words, table, length, new StringBuilder(length * 2));
+    return words;
+  }
+
+  private void findWords(
+    String s, ArrayList<String> words, ArrayList<List<Integer>> table, int level, StringBuilder sb) {
+
+    if (level <= 0) {
+      if (sb.length() > 0) {
+        words.add(sb.toString());
+      }
+      return;
+    }
+
+    for (int i: table.get(level)) {
+      boolean needSpace = sb.length() > 0;
+      if (needSpace) sb.insert(0, ' ');
+      String word = s.substring(i, level);
+      sb.insert(0, word);
+
+      findWords(s, words, table, i, sb);
+
+      sb.delete(0, word.length());
+      if (needSpace) sb.delete(0, 1);
+
+    }
+
   }
 }
