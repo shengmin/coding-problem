4f983c8bed26c787513a7fc5c304d89818e8f2b4
ShengMin Zhang <me@shengmin.me>
2014-03-22 00:32:17 -0400

    Solve substring search

--- hackerrank/substring-searching/Solution.scala
+++ hackerrank/substring-searching/Solution.scala
@@ -6,8 +6,10 @@ object Io {
     use(
       new BufferedReader(
         if (arguments.length == 0) new InputStreamReader(System.in)
-        else new FileReader(arguments(0))),
-      new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out))))(block)
+        else new FileReader(arguments(0))
+      ),
+      new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)))
+    )(block)
   }
 
   def use[A <: Closeable, B <: Closeable, R](resourceA: A, resourceB: B)(block: (A, B) => R): R = {
@@ -29,38 +31,42 @@ object Io {
 }
 
 object Solution {
-  private[this] def isMatch(text: String, pattern: String): Boolean = {
+  private[this] def buildFailureArray(pattern: String) = {
     val failure = new Array[Int](pattern.length)
     failure(0) = -1
-    for (i <- 2 to pattern.length) {
-      var j = failure(i - 1)
-      var toBreak = false
-      while (toBreak) {
-        if (pattern(j) == pattern(i - 1)) {
-          failure(i) = j + 1
-          toBreak = true
-        } else if (j == 0) {
-          failure(i) = 0
-          toBreak = true
-        } else {
-          j = failure(j)
-        }
+    var i = 2
+    var j = 0
+    while (i < pattern.length) {
+      if (pattern(i - 1) == pattern(j)) {
+        j += 1
+        failure(i) = j
+        i += 1
+      } else if (j > 0) {
+        j = failure(j)
+      } else {
+        failure(i) = 0
+        i += 1
       }
     }
+    failure
+  }
 
+  private[this] def isMatch(text: String, pattern: String): Boolean = {
+    val failure = buildFailureArray(pattern)
     var i = 0
     var j = 0
 
-    while (j < text.length) {
-      if (text(j) == pattern(i)) {
-        i += 1
-        j += 1
-        if (i == pattern.length)
+    while (j + i < text.length) {
+      if (pattern(i) == text(i + j)) {
+        if (i == pattern.length - 1)
           return true
-      } else if (i > 0) {
-        i = failure(i)
+        i += 1
       } else {
-        j += 1
+        j = j + i - failure(i)
+        if (failure(i) > -1)
+          i = failure(i)
+        else
+          i = 0
       }
     }
 
