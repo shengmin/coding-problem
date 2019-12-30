d9a560d932fcc891693abe953671e47c272bcdee
ShengMin Zhang <shengmin@fb.com>
2019-12-30 09:33:18 -0800

    [hr] a very big sum

---
+++ hackerrank/a-very-big-sum/Solution.kts
@@ -0,0 +1,37 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the aVeryBigSum function below.
+fun aVeryBigSum(ar: Array<Long>): Long {
+  return ar.reduce({sum, item -> sum + item})
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
+
+main(arrayOf())
