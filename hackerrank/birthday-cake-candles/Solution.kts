da676658979178f1ca17155439e9838348fbb66c
ShengMin Zhang <shengmin@fb.com>
2019-12-30 10:36:30 -0800

    [hr] birthday cake candles

---
+++ hackerrank/birthday-cake-candles/Solution.kts
@@ -0,0 +1,38 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the birthdayCakeCandles function below.
+fun birthdayCakeCandles(ar: Array<Int>): Int {
+  val maxHeight = ar.max()
+  return ar.count({height -> height == maxHeight})
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(ar)
+
+    println(result)
+}
+
+main(arrayOf())
