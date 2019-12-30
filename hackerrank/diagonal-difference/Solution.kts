b2906e70e28b07684924c05b256b8b4adb9b4dca
ShengMin Zhang <shengmin@fb.com>
2019-12-30 09:59:01 -0800

    [hr] diagonal-difference

---
+++ hackerrank/diagonal-difference/Solution.kts
@@ -0,0 +1,52 @@
+import java.io.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+import kotlin.math.*
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+  var topLeftToBottomRight = 0
+  var topRightToBottomLeft = 0
+
+  for (i in 0 until arr.size) {
+    topLeftToBottomRight += arr[i][i]
+    topRightToBottomLeft += arr[i][arr.size - i - 1]
+  }
+  return (topLeftToBottomRight - topRightToBottomLeft).absoluteValue
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
+
+main(arrayOf())
